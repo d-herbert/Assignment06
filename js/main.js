236ace9d3f1ab37d71a3aecf0158942d9e93f246
d-herbert <deidre.m.herbert@gmail.com>
2022-03-31 19:59:16 -0700

    initial popup working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // HANDLE THE LOAD EVENT OF THE WINDOW
-
-// HANDLE THE CLICK EVENT FOR THE BTNADDEMPLOYEE BUTTON
-
-// OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
+window.onload = function() {
+    // HANDLE THE CLICK EVENT FOR THE BTNADDEMPLOYEE BUTTON
+    document.getElementById('btnAddEmployee').addEventListener('click', () => {
+        // OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
+        window.open('add-employee.html','emp')
+    })
+}
