b3875ab8b780b16477b78cc008f82b9164affb2c
d-herbert <deidre.m.herbert@gmail.com>
2022-03-31 20:42:37 -0700

    initial popup elements created

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // HANDLE THE LOAD EVENT OF THE WINDOW
 window.onload = function() {
     // HANDLE THE CLICK EVENT FOR THE BTNADDEMPLOYEE BUTTON
-    document.getElementById('btnAddEmployee').addEventListener('click', () => {
+    window.document.getElementById('btnAddEmployee').addEventListener('click', () => {
         // OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
-        window.open('add-employee.html','emp')
+        window.open('add-employee.html','emp', 'width=100, height=100')
     })
 }
